ecd8f848646c9ec0974d58ce38594298e4a5a711
Gift Onwuanaku <giftonwuanaku@gmail.com>
2022-04-11 20:06:31 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print all the digits of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+char x;
+for (x = '0'; x <= '9'; ++X)
+putchar(x);
+for (x = 'a'; x <= 'f'; ++x)
+putchar(x);
+putchar('\n');
+return (0);
+}
