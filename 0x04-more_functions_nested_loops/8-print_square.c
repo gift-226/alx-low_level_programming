3e610c735049dacaacce2ef9fe160a77b9a254d1
Gift Onwuanaku <giftonwuanaku@gmail.com>
2022-04-14 19:30:01 -0700

    task 008

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_square - prints hashtags to make a square
- * @size: limit
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
  *
  * Return: void
  */
@@ -17,7 +17,7 @@ else
 {
 int i, j;
 
-for (i = 0; 1 < size; i++)
+for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
