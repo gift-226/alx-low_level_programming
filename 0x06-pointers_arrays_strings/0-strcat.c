fa5c4035faa327c6db24345fc7838f12fca6e0e8
Gift Onwuanaku <giftonwuanaku@gmail.com>
2022-04-25 16:03:43 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * * _strcat - concatenates two strings
+ * _strcat - concatenates two strings
  * @dest: input parameter string
  * @src: input parameter string
  *
@@ -13,18 +13,19 @@ int b;
 
 a = 0
 
-while (dest[a] != 0)
+while (dest[a] != '\0')
 {
 a++;
 }
 
 b = 0;
 
-while (src[b] != 0)
+while (src[b] != '\0')
 {
 dest[a] = src[b];
 a++;
 b++;
 }
+dest[a] = '\0';
 return (dest);
 }
