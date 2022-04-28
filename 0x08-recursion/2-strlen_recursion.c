b711c8f6f8284c37568a5976920751785517c190
Gift Onwuanaku <giftonwuanaku@gmail.com>
2022-04-28 16:37:26 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strlen_recursion - returns the length of a string.
+ * _strlen_recursion - returns the length of a string.
  * @s: string
  * Return: the length of a string.
  */
